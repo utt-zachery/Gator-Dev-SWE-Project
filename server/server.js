38ef0220b17a664e1281d172f79d1b86ac4e9477
Zachery Utt <55007266+utt-zachery@users.noreply.github.com>
2020-11-09 16:11:09 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 /* serve static files - see http://expressjs.com/en/starter/static-files.html */
-app.use(express.static('../'));
+app.use(express.static(path.join(__dirname, '../')));
 
 /* The next three middleware are important to the API that we are building */
 
