9c6e45a3576fd65c3a1362ff4b063fd10e603b97
Zachery Utt <55007266+utt-zachery@users.noreply.github.com>
2020-11-09 15:10:40 -0500

    Update foodBankModel.js

--- server/models/foodBankModel.js
+++ server/models/foodBankModel.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const foodBankSchema = new mongoose.Schema({
 
-    id: {type: Number, required: true },
     name: { type: String, required: true },
     address: { type: String, required: true },
     longitude: { type: Number, required: true },
