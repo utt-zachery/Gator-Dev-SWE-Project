5ba2839e2cb4ef0cd8758576e85931c5143c44fc
Zachery Utt <55007266+utt-zachery@users.noreply.github.com>
2020-11-10 12:12:38 -0500

    Changed name

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import * as foodBankController from '../controllers/foodBankController.js';
-import * as foodItemController from '../controllers/foodItemController.js';
+import * as donateItemController from '../controllers/donateItemController.js';
 import * as userController from '../controllers/userController.js'
 const apiRouter = express.Router();
 
 apiRouter.get('/getFoodBanks', foodBankController.viewFoodBanks);
 apiRouter.post('/addFoodBank', foodBankController.addFoodBank);
 
-apiRouter.post('/donateItem', foodItemController.donateItem);
+apiRouter.post('/donateItem', donateItemController.donateItem);
 apiRouter.post('/addUser', userController.newUser);
 
 export default apiRouter;
