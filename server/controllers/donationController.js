2aa3a719b0353e576169e9e1c6ee097061e1d346
utt-zachery <55007266+utt-zachery@users.noreply.github.com>
2020-11-11 22:19:59 -0500

    Update donationController.js

--- server/controllers/donationController.js
+++ server/controllers/donationController.js
@@ -1,4 +1,24 @@
 import Donation from '../models/donationModel.js'
+import FoodItem from '../models/foodModel.js'
+
+export const resolveFoodItems = async(data, req, res) => {
+    let donations = [];
+    for (let i =0; i < data.length; i++) {
+        await FoodItem.findById(data[i].foodItemID).then((foodItem)=> {
+            let arrayItem = {
+                quantity: data[i].quantity,
+                donationDate: data[i].donationDate,
+                foodItem: foodItem
+            }
+            donations.push(arrayItem);
+
+            if (i == data.length -1)
+            res.send(donations);
+        }).catch((err) => {
+            res.write(200).send(err);
+        });
+    }
+};
 
 export const viewDonations = async(req, res) => {
     if (!req.query || !req.query.userID) {
@@ -14,7 +34,7 @@ export const viewDonations = async(req, res) => {
     }
 
     await Donation.find({userID: req.query.userID}).sort({"donationDate": -1}).then((data) => {
-        return res.json(data);
+        return resolveFoodItems(data, req, res);
     }).catch((err) => {
         return res.status(200).send({
             message:  err
